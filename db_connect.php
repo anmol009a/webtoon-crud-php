f652d13ecad888653b7d8d827f715c2eea9f9d27
Anmol Sharma <anmolsharma009a@gmail.com>
2022-11-06 01:27:54 +0530

    added funtions and improves code

--- db_connect.php
+++ db_connect.php
@@ -12,6 +12,27 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 // Die if connection was not successful
 if (!$conn) {
     die("Sorry we failed to connect: " . mysqli_connect_error());
-} 
+}
 
-var_export($conn);
+class DatabaseConnection
+{
+    // Connecting to the Database
+    protected $servername = "localhost";
+    protected $username = "root";
+    protected $password = "";
+    protected $database = "mydb";
+
+    /**
+     * Create a connection
+     */
+    function db_connect()
+    {
+        $this->_db_connect = mysqli_connect($this->servername, $this->username, $this->password, $this->database);
+
+        // Die if connection was not successful
+        if (!$this->_db_connect) {
+            die("Sorry we failed to connect: " . mysqli_connect_error());
+        }
+        return $this->_db_connect;
+    }
+}
--- index.php
+++ index.php
@@ -1,184 +1,173 @@
 <?php
 
+require_once('db_connect.php');
+
 class WebtoonCrud
 {
+	protected $w_id;
+	protected $title;
+	protected $url;
+	protected $cover_url;
+	protected $number;
+	protected $chapter_url;
 
-    function __constructor()
-    {
-    }
+	/**
+	 * constructor
+	 * @param $conn takes mysql db connection object
+	 */
+	public function __construct(object $conn)
+	{
+		// object which represents the connection to a MySQL Server
+		$this->connection = $conn;
+	}
 
-    function insert_webtoons(array $webtoon_data)
-    {
-        include_once "db_connect.php";
+	function insert_webtoons(array $webtoon_data)
+	{
+		// sql stmt
+		$sql = "INSERT IGNORE INTO webtoons (title, url)  VALUES (?, ?)";
+		// prepare stmt
+		$stmt = $this->connection->prepare($sql);
+		// bind parameters
+		$stmt->bind_param("ss", $this->title, $this->url);
 
-        $sql = "INSERT IGNORE INTO webtoons (title, url)  VALUES (?, ?)";
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param("ss", $title, $url);
+		//for each webtoon data execute sql 
+		foreach ($webtoon_data as $webtoon) {
+			$this->title = $webtoon->title;
+			$this->url = $webtoon->url;
+			$stmt->execute();
+		}
+	}
 
-        foreach ($webtoon_data as $webtoon) {
-            $title = $webtoon->title;
-            $url = $webtoon->url;
-            $stmt->execute();
-            // try {
-            // $stmt->execute();
-            //     echo "Title: " . $title;
-            //     echo "<br>";
-            // } catch (Exception $e) {
-            //     echo "Failed to insert webtoon details : " . $e->getMessage();
-            //     echo "<br>";
-            // }
-        }
-    }
+	function insert_chapters(array $webtoon_data)
+	{
+		// define sql stmt
+		$sql = "INSERT INTO chapters (w_id, number, url)  VALUES (?, ?, ?)";
+		$stmt = $this->connection->prepare($sql);
+		$stmt->bind_param("ids", $this->w_id, $this->number, $this->url); // bind parameters
 
-    function insert_chapters(array $webtoon_data)
-    {
-        // create connection
-        include_once "db_connect.php";
+		// for each webtoon
+		foreach ($webtoon_data as $webtoon) {
 
-        // define sql stmt
-        $sql = "INSERT INTO chapters (w_id, number, url)  VALUES (?, ?, ?)";
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param("ids", $w_id, $number, $url); // bind parameters
+			$this->w_id = $webtoon->id;   // webtoon id
 
-        // for each webtoon
-        foreach ($webtoon_data as $webtoon) {
+			// for every chapter
+			foreach ($webtoon->chapters as $chapter) {
+				$this->number = $chapter->number; // chapter number
+				$this->url = $chapter->url;   // chapter url;
+				$stmt->execute();   // execute query
+			}
+		}
+	}
 
-            $w_id = $webtoon->id;   // webtoon id
+	function insert_covers(array $webtoon_data)
+	{
+		// define sql stmt
+		$sql = "INSERT INTO covers (w_id, url)  VALUES (?, ?)";
+		$stmt = $this->connection->prepare($sql);
+		$stmt->bind_param("is", $this->w_id, $this->cover_url); // bind parameters
 
-            // for every chapter
-            foreach ($webtoon->chapters as $chapter) {
-                $number = $chapter->number; // chapter number
-                $url = $chapter->url;   // chapter url;
-                $stmt->execute();   // execute query
-            }
-        }
-    }
+		// for each webtoon insert cover
+		foreach ($webtoon_data as $webtoon) {
+			$this->w_id = $webtoon->id;   // webtoon id
+			$this->cover_url = $webtoon->cover_url;   // cover url
+			$stmt->execute();   // execute query
+		}
+	}
 
-    function insert_covers(array $webtoon_data)
-    {
-        // create connection
-        include_once "db_connect.php";
+	/**
+	 * @return array of associated arrays
+	 */
+	function get_webtoons()
+	{
+		// define sql stmt
+		$sql = "SELECT * FROM webtoons ORDER BY updated_at DESC LIMIT 30";
 
-        // define sql stmt
-        $sql = "INSERT INTO covers (w_id, url)  VALUES (?, ?)";
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param("is", $w_id, $cover_url); // bind parameters
+		// execute query
+		$result = mysqli_query($this->connection, $sql);
 
-        // for each webtoon insert cover
-        foreach ($webtoon_data as $webtoon) {
-            $w_id = $webtoon->id;   // webtoon id
-            $cover_url = $webtoon->cover_url;   // cover url
-            $stmt->execute();   // execute query
-        }
-    }
+		// fetch result
+		$rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-    /**
-     * @return array of associated arrays
-     */
-    function get_webtoons()
-    {
-        // create connection
-        include_once "db_connect.php";
+		foreach ($rows as  $key => $row) {
+			$rows[$key]['chapters'] = $this->get_chapters($row['id']);
+		}
 
-        // define sql stmt
-        $sql = "SELECT * FROM webtoons ORDER BY updated_at DESC LIMIT 30";
+		// return an array of associated arrays
+		return $rows;
+	}
 
-        // execute query
-        $result = mysqli_query($conn, $sql);
+	/**
+	 * @return array of associated arrays
+	 */
+	function get_chapters(int $w_id)
+	{
 
-        // fetch result
-        $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+		// define sql stmt
+		$sql = "SELECT * FROM chapters WHERE w_id = $w_id ORDER BY 'number' DESC LIMIT 2";
 
-        foreach($rows as $row){
-            $chapters = $this->get_chapters($row['id']);
-            array_push($row, $chapters);
-        }
+		// execute query
+		$result = mysqli_query($this->connection, $sql);
 
-        print_r($rows);
+		// fetch result
+		$rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-        // return an array of associated arrays
-        return $rows;
-    }
+		// return an array of associated arrays
+		return $rows;
+	}
 
-    /**
-     * @return array of associated arrays
-     */
-    function get_chapters(int $w_id)
-    {
-        // create connection
-        include_once "db_connect.php";
+	/**
+	 * @return array of associated arrays
+	 */
+	function search_webtoon(string $query)
+	{
+		// define sql stmt
+		$sql = "SELECT * FROM webtoons WHERE title LIKE '%$query%' ORDER BY updated_at DESC LIMIT 30";
 
-        // define sql stmt
-        $sql = "SELECT * FROM chapters WHERE w_id = $w_id ORDER BY 'number' DESC LIMIT 2";
+		// execute query
+		$result = mysqli_query($this->connection, $sql);
 
-        // execute query
-        $result = mysqli_query($conn, $sql);
+		// fetch result
+		$rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-        // fetch result
-        $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
-        // print_r($rows);
-
-        // return an array of associated arrays
-        return $rows;
-    }
-
-    /**
-     * @return array of associated arrays
-     */
-    function search_webtoon(string $query)
-    {
-        // create connection
-        include_once "db_connect.php";
-
-        // define sql stmt
-        $sql = "SELECT * FROM webtoons WHERE title LIKE '%$query%' ORDER BY updated_at DESC LIMIT 30";
-
-        // execute query
-        $result = mysqli_query($conn, $sql);
-
-        // fetch result
-        $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
-        // return an array of associated arrays
-        return $rows;
-    }
+		// return an array of associated arrays
+		return $rows;
+	}
 }
 
 
-$webtoon_data = [
-    (object) [
-        'id' => '85',
-        'chapters' => [
-            (object)[
-                'number' => 23,
-                'url' => '23.html'
-            ]
-        ],
-        'title' => 'test11',
-        'url' => 'test11.html',
-        'cover_url' => 'test11.png'
-    ],
-    (object) [
-        'id' => '86',
-        'chapters' => [
-            (object)[
-                'number' => 20,
-                'url' => '20.html'
-            ]
-        ],
-        'title' => 'test12',
-        'url' => 'test12.html',
-        'cover_url' => 'test12.png'
-    ]
-];
+// $webtoon_data = [
+// 	(object) [
+// 		'id' => '85',
+// 		'chapters' => [
+// 			(object)[
+// 				'number' => 23,
+// 				'url' => '23.html'
+// 			]
+// 		],
+// 		'title' => 'test11',
+// 		'url' => 'test11.html',
+// 		'cover_url' => 'test11.png'
+// 	],
+// 	(object) [
+// 		'id' => '86',
+// 		'chapters' => [
+// 			(object)[
+// 				'number' => 20,
+// 				'url' => '20.html'
+// 			]
+// 		],
+// 		'title' => 'test12',
+// 		'url' => 'test12.html',
+// 		'cover_url' => 'test12.png'
+// 	]
+// ];
 
-$query = "a";
+// $query = "a";
 
-$a = new WebtoonCrud;
+// $a = new WebtoonCrud($conn);
 // $a->insert_webtoons($webtoon_data);
 // $a->insert_chapters($webtoon_data);
 // $a->insert_covers($webtoon_data);
-// $a->get_webtoons($webtoon_data);
-// $a->search_webtoon($query);
-// $a->get_chapters(1);
+// echo json_encode($a->get_webtoons($webtoon_data));
+// echo json_encode($a->get_chapters(1));
+// echo json_encode($a->search_webtoon($query));
